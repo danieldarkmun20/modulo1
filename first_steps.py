0f1d4f505e8bc439532ed12320bcc46ee206bbe7
DanielDarkmun <dape_18@alu.uabcs.mx>
2022-02-06 16:39:19 -0700

    Actividad 2 finalizada

--- first_steps.py
+++ first_steps.py
@@ -1,10 +1,4 @@
-#First exercise
-from datetime import date;
-print("Today's date is: " + str(date.today()))
-
-#Second exercise
 parsec = 11
-
-lightyears = parsec * 3.26156;
+lightyears = parsec * 3.26156
 
 print(str(parsec) + " parsec, is " + str(lightyears) + " lightyears")
